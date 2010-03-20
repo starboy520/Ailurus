afe19673001f7b0882e5bad4891c0e48922f0710
Homer Xing <homer.xing@gmail.com>
2010-03-20 21:59:31 +0800

    Try to fix launch issue

---
+++ ailurus/__init__.py
@@ -0,0 +1,9 @@
+import info_pane
+import install_remove_pane
+import lib
+import libapp
+import libsetting
+import libu
+import loader
+import main
+import system_setting_pane
---
+++ ailurus/support/__init__.py
@@ -0,0 +1,10 @@
+import multidragview
+import pangobuffer
+import releasenotesviewer
+import searchbox
+import splashwindow
+import terminal_single_thread
+import terminal
+import tipoftheday
+import undobuffer
+import windowpos
