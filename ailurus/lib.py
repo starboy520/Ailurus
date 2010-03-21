4660c202c1e4a90b07b74d35be8b908d2ae71118
Homer Xing <homer.xing@gmail.com>
2010-03-21 11:50:40 +0800

    Exclude debian/ from tar.gz

--- ailurus/lib.py
+++ ailurus/lib.py
@@ -20,8 +20,8 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
 from __future__ import with_statement
-AILURUS_VERSION = '10.03.1.5'
-AILURUS_RELEASE_DATE = '2010-03-18'
+AILURUS_VERSION = '10.03.1.8'
+AILURUS_RELEASE_DATE = '2010-03-21'
 D = '/usr/share/ailurus/data/'
 import warnings
 warnings.filterwarnings("ignore", "apt API not stable yet", FutureWarning)
