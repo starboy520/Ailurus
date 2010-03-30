5322774a32ce0d4b87095e69a599868638af5179
Homer Xing <homer.xing@gmail.com>
2010-03-30 12:06:35 +0800

    Add icon for "clean up" pane.

--- ailurus/common/menu.py
+++ ailurus/common/menu.py
@@ -98,6 +98,7 @@ firestarter.png is copied from Firestarter project. It is released under the GPL
 gcompris.png is copied from GCompris project. It is released under the GPL license. Its copyright is holded by Bruno Coudoin.
 liferea.png is copied from Liferea project. It is released under the GPL license. Its copyright is holded by Liferea Team.
 stardict.png is copied from Stardict project. It is released under GPL v3 license. Its copyright is holded by Stardict Team.
+m_clean_up.png is released under the GPL license. Its copyright is holded by MA Yue.
 netbeans.png is copied from Netbeans project. It is released under the GPL v2 license. Its copyright is holded by Sun Microsystems Ltd.
 python.png is copied from Python project. It is released under the Python license. Its copyright is holded by Python Software Foundation.
 qtcreator.png is copied from Qt project. It is released under the LGPL license. Its copyright is holded by Nokia Corporation.
