c70f7d42b3c4b54f11e6957bad9d3ba38d2b8a50
Homer Xing <homer.xing@gmail.com>
2010-04-24 16:56:21 +0800

    Add missing with_statement

--- ailurus/ubuntu/quick_setup.py
+++ ailurus/ubuntu/quick_setup.py
@@ -19,6 +19,7 @@
 # along with Ailurus; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
+from __future__ import with_statement
 import sys, os
 import ailurus
 sys.path.insert(0, os.path.dirname(os.path.abspath(ailurus.__file__)))
