ded8760040546eea1ec5e707b7badb350813c429
velly <velly.empire@gmail.com>
2010-05-04 11:18:16 +0800

    firefox beta 2

--- ailurus/common/apps_eclipse.py
+++ ailurus/common/apps_eclipse.py
@@ -4,6 +4,7 @@
 # Ailurus - make Linux easier to use
 #
 # Copyright (C) 2007-2010, Trusted Digital Technology Laboratory, Shanghai Jiao Tong University, China.
+# Copyright (C) 2009-2010, Ailurus Developers Team
 #
 # Ailurus is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,7 +23,7 @@
 from __future__ import with_statement
 import sys, os
 from lib import *
-from libapp import * 
+from libapp import *
 
 def create_eclipse_icon():
         memarg = ''
@@ -66,11 +67,11 @@ def message(title, content):
 if Config.is_Fedora():
     class Eclipse(_rpm_install):
         __doc__ = _('Eclipse (basic development environment)')
-        detail = ( 
+        detail = (
                 _('Eclipse is from http://www.eclipse.org/downloads/ \n') +
                 _('You can install Language pack according to the instructions on the page http://www.eclipse.org/babel/downloads.php\n'
                   'You can download Language pack from http://download.eclipse.org/technology/babel/babel_language_packs/ganymede.php, '
-                  'and extract ".zip" file to directory "/opt/eclipse" .') + 
+                  'and extract ".zip" file to directory "/opt/eclipse" .') +
                 _(' This application depends on Java.') )
         category = 'eclipse'
         license = EPL + ' http://www.eclipse.org/org/documents/epl-v10.php'
@@ -78,11 +79,11 @@ if Config.is_Fedora():
 elif Config.is_Ubuntu():
     class Eclipse(_apt_install):
         __doc__ = _('Eclipse (basic development environment)')
-        detail = ( 
+        detail = (
                 _('Eclipse is from http://www.eclipse.org/downloads/ \n') +
                 _('You can install Language pack according to the instructions on the page http://www.eclipse.org/babel/downloads.php\n'
                   'You can download Language pack from http://download.eclipse.org/technology/babel/babel_language_packs/ganymede.php, '
-                  'and extract ".zip" file to directory "/opt/eclipse" .') + 
+                  'and extract ".zip" file to directory "/opt/eclipse" .') +
                 _(' This application depends on Java.') )
         category = 'eclipse'
         license = EPL + ' http://www.eclipse.org/org/documents/epl-v10.php'
