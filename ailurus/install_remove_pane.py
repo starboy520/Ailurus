cf2aadee5d885dda439d08f0594038348282e974
Homer Xing <homer.xing@gmail.com>
2010-05-07 23:22:00 +0800

    Polish code

--- ailurus/install_remove_pane.py
+++ ailurus/install_remove_pane.py
@@ -706,13 +706,13 @@ class InstallRemovePane(gtk.VBox):
             item = [i1, icon(i2), i3]
             treestore.append(parent, item)
         
-        self.quick_setup_pane = quick_setup_pane = gtk.HBox(False, 10)
+        quick_setup_pane = gtk.HBox(False, 10)
         quick_setup_pane.set_border_width(5)
         quick_setup_button = image_file_button(_('Quickly install popular software').center(60), D + 'umut_icons/quick_setup.png', 24)
         quick_setup_button.connect('clicked', self.__launch_quick_setup)
         quick_setup_checkbutton = gtk.CheckButton(_('Hide'))
-        def hide_quick_setup(chkbtn):
-            Config.set_hide_quick_setup_pane(chkbtn.get_active())
+        def hide_quick_setup(button):
+            Config.set_hide_quick_setup_pane(button.get_active())
             notify(_('Preferences changed'), _('Your changes will take effect at the next time when the program starts up.'))
         quick_setup_checkbutton.connect('clicked', hide_quick_setup)
         quick_setup_pane.pack_start(quick_setup_button, False)
