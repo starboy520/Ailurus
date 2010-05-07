be385149b4f195a6c8c34055192824cf64be6d2a
Homer Xing <homer.xing@gmail.com>
2010-05-07 14:08:46 +0800

    Sort code

--- ailurus/ubuntu/app_tasksel.py
+++ ailurus/ubuntu/app_tasksel.py
@@ -25,6 +25,20 @@ import sys, os
 from lib import *
 from libapp import *
 
+class Launch_Tasksel(I):
+    __doc__ = _('* Launch tasksel')
+    detail = _('This is a helper item. It just launches command: "sudo tasksel". '
+               'Then you are free to customize your computer via "tasksel".')
+    category = 'server'
+    def installed(self):
+        return False
+    def install(self):
+        if not APT.installed('tasksel'): APT.install('tasksel')
+        run_as_root_in_terminal('tasksel')
+        APT.cache_changed()
+    def remove(self):
+        raise NotImplementedError
+
 class _tasksel(I):
     category = 'server'
     def install(self):
@@ -38,17 +52,37 @@ class _tasksel(I):
     def installation_command(self):
         return _('Command:') + ' sudo tasksel install ' + self.name
 
-class Tasksel_LAMP_server(_tasksel):
-    __doc__ = _('LAMP: Install Apache2 + MySQL + PHP')
-    name = 'lamp-server'
-
 class Tasksel_DNS_server(_tasksel):
     __doc__ = _('DNS server')
     name = 'dns-server'
 
+class Tasksel_Eucalyptus_node(_tasksel):
+    __doc__ = _('Eucalyptus cloud computing node')
+    name = 'eucalyptus-node'
+
+class Tasksel_Eucalyptus_simple_cluster(_tasksel):
+    __doc__ = _('Eucalyptus cloud computing cluster')
+    name = 'eucalyptus-simple-cluster'
+        
+class Tasksel_LAMP_server(_tasksel):
+    __doc__ = _('LAMP: Install Apache2 + MySQL + PHP')
+    name = 'lamp-server'
+
 class Tasksel_Mail_server(_tasksel):
     __doc__ = _('Mail server')
     name = 'mail-server'
+
+class Tasksel_Mobile_Live(_tasksel):
+    __doc__ = _('Ubuntu MID Live')
+    name = 'mobile-live'
+
+class Tasksel_Mobile_MID(_tasksel):
+    __doc__ = _('Ubuntu MID')
+    name = 'mobile-mid'
+
+class Tasksel_Mobile_netbook_remix(_tasksel):
+    __doc__ = _('Ubuntu Netbook')
+    name = 'mobile-netbook-remix'
 
 class Tasksel_Openssh_server(_tasksel):
     __doc__ = _('OpenSSH server')
@@ -70,9 +104,9 @@ class Tasksel_Tomcat_server(_tasksel):
     __doc__ = _('Tomcat Java server')
     name = 'tomcat-server'
 
-class Tasksel_Ubuntustudio_graphics(_tasksel):
-    __doc__ = _('Ubuntustudio-graphics: 2D/3D creation and editing')
-    name = 'ubuntustudio-graphics'
+class Tasksel_UEC(_tasksel):
+    __doc__ = _('Ubuntu Enterprise Cloud server')
+    name = 'uec'
 
 class Tasksel_Ubuntustudio_audio(_tasksel):
     __doc__ = _('Ubuntustudio-audio: Audio creation and editing')
@@ -82,48 +116,14 @@ class Tasksel_Ubuntustudio_audio_plugins(_tasksel):
     __doc__ = _('Ubuntustudio-audio-plugins: LADSPA and DSSI audio creation and editing')
     name = 'ubuntustudio-audio-plugins'
 
-class Tasksel_Mobile_MID(_tasksel):
-    __doc__ = _('Ubuntu MID')
-    name = 'mobile-mid'
-
-class Tasksel_Mobile_netbook_remix(_tasksel):
-    __doc__ = _('Ubuntu Netbook')
-    name = 'mobile-netbook-remix'
-
-class Tasksel_Ubuntustudio_video(_tasksel):
-    __doc__ = _('Ubuntustudio-video: Video creation and editing')
-    name = 'ubuntustudio-video'
-
-class Tasksel_Mobile_Live(_tasksel):
-    __doc__ = _('Ubuntu MID Live')
-    name = 'mobile-live'
-
-class Tasksel_Eucalyptus_simple_cluster(_tasksel):
-    __doc__ = _('Eucalyptus cloud computing cluster')
-    name = 'eucalyptus-simple-cluster'
-        
-class Tasksel_Eucalyptus_node(_tasksel):
-    __doc__ = _('Eucalyptus cloud computing node')
-    name = 'eucalyptus-node'
-
-class Tasksel_UEC(_tasksel):
-    __doc__ = _('Ubuntu Enterprise Cloud server')
-    name = 'uec'
-
 class Tasksel_Ubuntustudio_font_meta(_tasksel):
     __doc__ = _('Ubuntustudio-font-meta: A lot of font')
     name = 'ubuntustudio-font-meta'
 
-class Launch_Tasksel(I):
-    __doc__ = _('* Launch tasksel')
-    detail = _('This is a helper item. It just launches command: "sudo tasksel". '
-               'Then you are free to customize your computer via "tasksel".')
-    category = 'server'
-    def installed(self):
-        return False
-    def install(self):
-        if not APT.installed('tasksel'): APT.install('tasksel')
-        run_as_root_in_terminal('tasksel')
-        APT.cache_changed()
-    def remove(self):
-        raise NotImplementedError
+class Tasksel_Ubuntustudio_graphics(_tasksel):
+    __doc__ = _('Ubuntustudio-graphics: 2D/3D creation and editing')
+    name = 'ubuntustudio-graphics'
+
+class Tasksel_Ubuntustudio_video(_tasksel):
+    __doc__ = _('Ubuntustudio-video: Video creation and editing')
+    name = 'ubuntustudio-video'
