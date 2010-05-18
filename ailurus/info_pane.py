b1b84302c57ad100cda481b78a9fa6598db293c3
Homer Xing <homer.xing@gmail.com>
2010-05-18 19:23:09 +0800

    Add ailurus/computer_doctor_pane

--- ailurus/info_pane.py
+++ ailurus/info_pane.py
@@ -21,7 +21,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
 from __future__ import with_statement
-import gtk, traceback, sys, os
+import gtk, sys, os
 from lib import *
 from libu import *
 
