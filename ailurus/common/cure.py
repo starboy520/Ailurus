4a06cceececcecc758a85d3d4c96e26ecab384ad
Homer Xing <homer.xing@gmail.com>
2010-05-18 18:47:57 +0800

    Add  Autostart_Workrave()

--- ailurus/common/cure.py
+++ ailurus/common/cure.py
@@ -22,5 +22,36 @@
 
 from __future__ import with_statement
 import sys, os
+if __name__ == '__main__':
+    sys.path.insert(0, '/home/ds/workspace/Ailurus/ailurus')
 from lib import *
 
+class Autostart_Workrave(C):
+    __doc__ = _('Automatically start up Workrave')
+    path = os.path.expanduser('~/.config/autostart/')
+    file = path + 'workrave.desktop'
+    def exists(self):
+        if UBUNTU or MINT:
+            if APT.installed('workrave') and not os.path.exists(self.file):
+                return True
+        if FEDORA:
+            if RPM.installed('workrave') and not os.path.exists(self.file):
+                return True
+        return False
+    def cure(self):
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+        with open(self.file, 'w') as f:
+            f.write('[Desktop Entry]\n'
+                    'Name=Workrave\n'
+                    'Exec=workrave\n'
+                    'Encoding=UTF-8\n'
+                    'Version=1.0\n'
+                    'Type=Application\n'
+                    'X-GNOME-Autostart-enabled=true\n')
+
+if __name__ == '__main__':
+    obj = Autostart_Workrave()
+    assert obj.exists()
+    obj.cure()
+    assert False == obj.exists()
