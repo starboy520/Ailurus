292d740deeed13859d948881ab5e967fc74253e4
Homer Xing <homer.xing@gmail.com>
2010-05-19 16:30:52 +0800

    Move some cure classes to subdir

--- ailurus/fedora/cure.py
+++ ailurus/fedora/cure.py
@@ -20,9 +20,18 @@
 # along with Ailurus; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
-import apps
-import cure
-import menu
-import osinfo
-import setting
-import tips
+from __future__ import with_statement
+import sys, os
+from lib import *
+
+class Colorful_BASH_prompt_symbols(C):
+    __doc__ = _('Use colorful Bash prompt symbols')
+    detail = (_('Add this line into ~/.bashrc:') + '\n' +
+              r"PS1='\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\W\[\033[00m\]\\$ '")
+    bashrc = os.path.expanduser('~/.bashrc')
+    line = r"PS1='\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\W\[\033[00m\]\\$ '"
+    def exists(self):
+        return not file_contain(self.bashrc, self.line)
+    def cure(self):
+        file_append(self.bashrc, self.line)
+        notify( _('The color of bash prompt symbols is changed.'), _('It will take effect at the next time you log in.') )
