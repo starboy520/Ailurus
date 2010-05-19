48db04926bf2189e0715aac120f8d232ff5a3e21
Homer Xing <homer.xing@gmail.com>
2010-05-19 08:04:20 +0800

    Add Add_user_to_vboxusers_group(C) & Fix_error_in_fontconfig_properties_Ubuntu(C)

--- ailurus/common/cure.py
+++ ailurus/common/cure.py
@@ -139,7 +139,7 @@ class Colorful_BASH_prompt_symbols_Ubuntu(C):
         notify( _('The color of bash prompt symbols is changed.'), _('It will take effect at the next time you log in.') )
 
 class Fix_error_in_49_sansserif_conf(C):
-    __doc__ = _('Fix errors in 49-sansserif.conf. Otherwise, characters in Flash may be displayed as blank diamond.')
+    __doc__ = _('Fix errors in 49-sansserif.conf. Otherwise, some character in Flash would be displayed as blank diamond.')
     detail = _('Change "sans-serif" to "sans serif".')
     type = C.MUST_FIX
     def exists(self):
@@ -154,6 +154,43 @@ class Fix_error_in_49_sansserif_conf(C):
                 content = f.read()
             content = content.replace('>sans-serif<', '>sans serif<')
             with open('/etc/fonts/conf.d/49-sansserif.conf', 'w') as f:
+                f.write(content)
+
+class Add_user_to_vboxusers_group(C):
+    __doc__ = _('Add yourself to "vboxusers" group. Otherwise, USB devices cannot be used in VirtualBox.')
+    detail = _('Command:') + ' gpasswd -a $USER vboxusers'
+    type = C.MUST_FIX
+    def exists(self):
+        username = os.environ['USER']
+        with open('/etc/group') as f:
+            lines = f.readlines()
+        for line in lines:
+            if line.startswith('vboxusers:'):
+                return not username in line
+    def cure(self):
+        command = os.path.expandvars('gpasswd -a $USER vboxusers')
+        run_as_root(command)
+
+class Fix_error_in_fontconfig_properties_Ubuntu(C):
+    __doc__ = _('Fix errors in Java font configuration. Otherwise, some unicode characters cannot be displayed.')
+    detail = _('Change wqy-zenhei.ttf to wqy-zenhei.ttc. Change uming.ttf to uming.ttc.')
+    type = C.MUST_FIX
+    file = '/etc/java-6-openjdk/fontconfig.properties'
+    def exists(self):
+        if not (UBUNTU or MINT): return False
+        try:
+            with open(self.file) as f:
+                content = f.read()
+            return '/wqy-zenhei.ttf' in content or '/uming.ttf' in content
+        except IOError:
+            return False
+    def cure(self):
+        with TempOwn(self.file) as o:
+            with open(self.file) as f:
+                content = f.read()
+            content = content.replace('/wqy-zenhei.ttf', '/wqy-zenhei.ttc')
+            content = content.replace('/uming.ttf', '/uming.ttc')
+            with open(self.file, 'w') as f:
                 f.write(content)
 
 # This class needs improvement
