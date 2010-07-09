3450f7043bf883c2bb0df6355c71fa0931e74878
Homer Xing <homer.xing@gmail.com>
2010-07-09 16:14:29 +0800

    Put developers before laboratory.

--- ailurus/support/checkupdate.py
+++ ailurus/support/checkupdate.py
@@ -136,8 +136,9 @@ def show_about_dialog():
           'M. Umut Pulat    http://12m3.deviantart.com/', 
           'Andrea Soragna   http://sora-meliae.deviantart.com/',
           'Paul Davey       http://mattahan.deviantart.com/',] )
-    about.set_copyright( _(u"Copyright © 2007-2010,\nTrusted Digital Technology Laboratory,\nShanghai Jiao Tong University, China.") + '\n'
-                         + _(u"Copyright © 2009-2010, Ailurus Developers Team") )
+    about.set_copyright(_(u"Copyright ©") + ' 2009-2010, ' + _('Ailurus developers and Ailurus contributors') + '\n' + 
+                        _(u"Copyright ©") + ' 2007-2010, ' + 
+                        _('Trusted Digital Technology Laboratory, Shanghai Jiao Tong University, China.'))
     about.set_wrap_license(False)
     about.set_license('''
 Ailurus is free software; you can redistribute it and/or modify
