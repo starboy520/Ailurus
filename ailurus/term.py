a2c0632e3b0d12c4aeda89ed1110cd5ae1985547
Homer Xing <homer.xing@gmail.com>
2010-07-20 20:41:58 +0800

    Add term.py, to substitude xterm.

---
+++ ailurus/term.py
@@ -0,0 +1,76 @@
+#coding: utf8
+#
+# Ailurus - make Linux easier to use
+#
+# Copyright (C) 2009-2010, Ailurus developers and Ailurus contributors
+# Copyright (C) 2007-2010, Trusted Digital Technology Laboratory, Shanghai Jiao Tong University, China.
+#
+# Ailurus is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Ailurus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ailurus; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+import os, sys, vte, gtk, StringIO, thread
+from lib import *
+
+class Window:
+    def __init__(self):
+        self.terminal = terminal = vte.Terminal()
+        terminal.connect('child-exited', lambda *w: self.child_exited()) 
+        self.scrollwindow = scroll = gtk.ScrolledWindow()
+        scroll.set_policy(gtk.POLICY_NEVER, gtk.POLICY_NEVER)
+        scroll.add_with_viewport(terminal)
+        self.window = window = gtk.Window()
+        gtk.window_set_default_icon_from_file(D+'suyun_icons/default.png')
+        window.set_title(_('Ailurus terminal'))
+        window.set_position(gtk.WIN_POS_CENTER)
+        window.add(scroll)
+        window.connect('delete-event', lambda *w: True)
+        window.show_all()
+        window.realize()
+    def child_exited(self):
+        exit_status = self.terminal.get_child_exit_status()
+        if exit_status:
+            sys.exit(1)
+        else:
+            sys.exit(0)
+    def run(self, argv): # please do not launch me as a thread
+        assert isinstance(argv, list)
+
+        # This idea comes from jhbuild/frontends/gtkui.py
+        # I wish to thank Project jhbuild!
+        env = {}
+        if Config.get_use_proxy():
+            try: 
+                proxy_string = get_proxy_string()
+                assert proxy_string
+            except: pass
+            else:
+                env = os.environ.copy()
+                env.update({'http_proxy':proxy_string,
+                            'https_proxy':proxy_string,
+                            'ftp_proxy':proxy_string,
+                            })
+        msg = StringIO.StringIO()
+        print >>msg, '\x1b[1;33m' + _('Run command:'), ' '.join(argv), '\x1b[m', '\r'
+        self.terminal.feed(msg.getvalue())
+        pid = self.terminal.fork_command(command=argv[0], argv=argv,
+                                         directory=os.getcwd(),
+                                         envv=['%s=%s' % x for x in env.items()],)
+        if pid == -1:
+            sys.exit(1)
+
+if __name__ == '__main__':
+    window = Window()
+    argv = sys.argv[1:]
+    window.run(argv)
+    gtk.main()
