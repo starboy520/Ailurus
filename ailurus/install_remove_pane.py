381f137787150c6a3a3a2bf8a81c3e54c13ea2e0
Homer Xing <homer.xing@gmail.com>
2010-07-27 21:12:13 +0800

    Use thread model for downloading icons.

--- ailurus/install_remove_pane.py
+++ ailurus/install_remove_pane.py
@@ -704,8 +704,8 @@ class InstallRemovePane(gtk.VBox):
         self.pack_start(self.status_label, False)
         self.show_all()
     
-        import gobject
-        gobject.timeout_add(1000, self.notify_sync)
+        import thread
+        thread.start_new_thread(self.notify_sync, ())
     
     def show_status(self):
         num = len(AppObjs.appobjs)
@@ -713,6 +713,7 @@ class InstallRemovePane(gtk.VBox):
         self.status_label.set_text(text)
     
     def notify_sync(self):
+        gtk.gdk.threads_enter()
         from download_icons import icons_pack_version
         if icons_pack_version > Config.get_last_synced_data_version():
             dialog = gtk.MessageDialog(buttons=gtk.BUTTONS_YES_NO,
@@ -722,11 +723,16 @@ class InstallRemovePane(gtk.VBox):
             dialog.destroy()
             if ret == gtk.RESPONSE_YES:
                 self.synchronize()
-
+        gtk.gdk.threads_leave()
+        
     def synchronize(self):
         import download_icons
         Config.set_last_synced_data_version(download_icons.icons_pack_version)
-        download_icons.main()
+
+        import subprocess
+        task = subprocess.Popen(['python', A+'/download_icons.py'])
+        task.wait()
+
         self.do_refresh_icon()
 
     def left_class_choose_button_clicked(self, button):
