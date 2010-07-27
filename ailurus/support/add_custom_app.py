333d2d8f99cb67c77c77a8d753257eda0727bb1b
Homer Xing <homer.xing@gmail.com>
2010-07-27 22:25:38 +0800

    improve

--- ailurus/support/add_custom_app.py
+++ ailurus/support/add_custom_app.py
@@ -175,15 +175,15 @@ class AddCustomAppDialog(gtk.Dialog):
         table.set_col_spacings(10)
         table.set_row_spacings(5)
         table.attach(label_name, 0, 1, 0, 1, gtk.FILL, 0)
-        table.attach(entry_name, 1, 2, 0, 1, gtk.FILL, 0)
+        table.attach(entry_name, 1, 2, 0, 1)
         table.attach(label_detail, 0, 1, 1, 2, gtk.FILL, 0)
-        table.attach(entry_detail, 1, 2, 1, 2, gtk.FILL, 0)
+        table.attach(entry_detail, 1, 2, 1, 2)
         table.attach(label_pkgs_to_install, 0, 1, 2, 3, gtk.FILL, 0)
-        table.attach(entry_pkgs, 1, 2, 2, 3, gtk.FILL, 0)
+        table.attach(entry_pkgs, 1, 2, 2, 3)
         table.attach(label_add_pkg_to_list, 0, 1, 3, 4, gtk.FILL, 0)
-        table.attach(hbox_add_pkg, 1, 2, 3, 4, gtk.FILL, 0)
+        table.attach(hbox_add_pkg, 1, 2, 3, 4)
         table.attach(label_category, 0, 1, 4, 5, gtk.FILL, 0)
-        table.attach(combo_category, 1, 2, 4, 5, gtk.FILL, 0)
+        table.attach(combo_category, 1, 2, 4, 5)
         
         # left_box
         self.icon_chooser = icon_chooser = ImageChooser('/usr/share/pixmaps/', 48, 48)
@@ -196,13 +196,13 @@ class AddCustomAppDialog(gtk.Dialog):
         # top_box
         top_box = gtk.HBox(False, 10)
         top_box.pack_start(left_vbox, False)
-        top_box.pack_start(table, False)
+        top_box.pack_start(table)
 
         gtk.Dialog.__init__(self,
                             title=_('Edit custom application'),
                             flags=gtk.DIALOG_MODAL|gtk.DIALOG_NO_SEPARATOR)
         self.set_position(gtk.WIN_POS_CENTER)
-        self.set_border_width(10)
+        self.set_border_width(5)
         self.vbox.set_spacing(10)
         self.vbox.pack_start(top_box, False)
         self.action_area.pack_start(self.__build_bottom_box(), False)
