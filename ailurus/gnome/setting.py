7b5ac9a0d2897116afd9417d061b919647b68be2
Homer Xing <homer.xing@gmail.com>
2010-10-29 13:42:42 +0800

    Bug

--- ailurus/gnome/setting.py
+++ ailurus/gnome/setting.py
@@ -385,7 +385,7 @@ def __nautilus_thumbnail_setting():
     o = gtk.Button(_('Clean thumbnail image cache') + ' (%s)' % _('command: rm -rf $HOME/.thumbnails/*'))
     def clean(*w):
         os.system('rm -rf $HOME/.thumbnails/*')
-        notify(_('Nautilus thumbnail image cache is clean'))
+        notify(_('Nautilus thumbnail image cache is clean'), ' ')
     o.connect('clicked', clean)
     table.attach(o, 0, 2, 4, 5, gtk.FILL, gtk.FILL)
     
