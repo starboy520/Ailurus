5e7106a622f54c42b1d693c75e4e573f35393e5f
velly <velly.empire@gmail.com>
2010-10-31 17:05:56 +0800

    image display

--- ailurus/libsetting.py
+++ ailurus/libsetting.py
@@ -184,12 +184,13 @@ class ImageChooser(gtk.Button):
         'If image_path is none, then show blank.'
         child = self.get_child()
         if child: self.remove(child)
-        if image_path==True & os.path.exists(image_path):
+        if os.path.exists(image_path):
             self.pixbuf = gtk.gdk.pixbuf_new_from_file(image_path).scale_simple(self.width, self.height, gtk.gdk.INTERP_HYPER)
         else:
             self.pixbuf = blank_pixbuf(self.width, self.height)
         self.add(gtk.image_new_from_pixbuf(self.pixbuf))
         self.show_all()
+        
     def display_pixbuf(self, pixbuf):
         'If image_path is none, then show blank.'
         child = self.get_child()
