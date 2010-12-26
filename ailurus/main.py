14b283bbe2b4d2c9b3938c865aeb1268fa9c0c64
Homer Xing <homer.xing@gmail.com>
2010-12-26 16:04:14 +0800

    Do not automatically change toolbar font size

--- ailurus/main.py
+++ ailurus/main.py
@@ -286,24 +286,6 @@ class toolitem(gtk.ToolItem):
         self.align_image.set_size_request(3*self.image_size/2, -1)
         self.align_image.show_all()
     
-    def refresh(self, size):
-        if self.image_size != size:
-            self.image_size = size
-            self.__load_image()
-            self.__change_font_size()
-    
-    def __change_font_size(self):
-        import pango
-        if self.image_size <= 25:
-            font_size = 4
-        elif 25 < self.image_size <= 30:
-            font_size = 5
-        elif 30 < self.image_size <= 40:
-            font_size = 6
-        else:
-            font_size = 8
-        self.text.modify_font(pango.FontDescription('Sans %s' % font_size))
-        
     def __init__(self, icon, text, signal_name, callback, *callback_args):
         gtk.ToolItem.__init__(self)
         
@@ -316,9 +298,10 @@ class toolitem(gtk.ToolItem):
         self.icon = icon
         self.align_image = align_image = gtk.Alignment(0.5, 0.5)
         self.__load_image()
-        self.text = text = gtk.Label(text)
+        label = gtk.Label()
+        label.set_markup('<small>%s</small>' % text)
+        self.text = text = label
         import pango
-        text.modify_font(pango.FontDescription('Sans 9'))
         text.set_alignment(0.5, 0.5)
         text.set_justify(gtk.JUSTIFY_CENTER)
         vbox = vbox = gtk.VBox(False, 5)
@@ -423,12 +406,6 @@ class MainView:
     def get_item_icon_size(self):
         return min( int(self.last_x / 20), 48)
 
-    def __refresh_toolbar(self):
-        icon_size = self.get_item_icon_size()
-        for i in range(0, self.toolbar.get_n_items()):
-            item = self.toolbar.get_nth_item(i)
-            item.refresh(icon_size)
-
     def __show_popupmenu_on_toolbaritem(self, widget, event, menu):
         if event.type == gtk.gdk.BUTTON_RELEASE and event.button == 1:
             def func(menu):
@@ -529,11 +506,6 @@ class MainView:
         self.window = window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         window.set_title('Ailurus')
         self.last_x = self.window.get_size()[0]
-        def configure_event(window, event, toolbar):
-            if self.last_x != self.window.get_size()[0]:
-                self.last_x = self.window.get_size()[0]
-                self.__refresh_toolbar()
-        self.window.connect('configure_event', configure_event, self.toolbar)
         window.connect("delete_event", self.terminate_program)
         window.add(vbox)
 
